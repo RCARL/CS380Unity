b8af24d1f2b29ae33ad144fa4b8c6ab35d6cf180
stephen <watchformorning@gmail.com>
2014-03-05 17:29:56 -0500

    now without using prefabs
    be sure to link proper texture in unity

--- Assets/voxels/world.cs
+++ Assets/voxels/world.cs
@@ -3,12 +3,14 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class world : MonoBehaviour {
 
 	public GameObject[,,] chunks;
 	int chunkSize =5;
 	public GameObject chunk;
 	private byte[,,] blocks=new byte[15,15,5];
+	public Texture primTexture;
 	void BuildMesh(){
 
 		for (int x=0; x<15; x++){
@@ -28,17 +30,28 @@ public class world : MonoBehaviour {
 		blocks[3,3,3]=1;
 		
 	}
+	private GameObject createChunk(float x,float y, float z)
+	{
+		GameObject ans = new GameObject (x + " " + y + " " + z);
+		ans.AddComponent ("MeshCollider");
+		ans.AddComponent ("MeshRenderer");
+		ans.AddComponent ("MeshFilter");
+		ans.AddComponent ("prim");
+		ans.transform.parent=gameObject.transform;
+		ans.transform.position = new Vector3 (x * chunkSize - 0.5f, y * chunkSize + 0.5f, z * chunkSize - 0.5f);
+		ans.transform.rotation = new Quaternion (0, 0, 0, 0);
+		ans.renderer.material.mainTexture = primTexture;
+		return ans;
+
+	}
 	void Start () {
 
 		chunks= new GameObject[2,2,2];
 			for (int x=0; x<chunks.GetLength(0); x++) 
 				for (int y=0; y<chunks.GetLength(1); y++) 
 					for (int z=0; z<chunks.GetLength(2); z++) {
-						chunks [x, y, z] = Instantiate (chunk,
-		                          new Vector3 (x * chunkSize - 0.5f, y * chunkSize + 0.5f, z * chunkSize - 0.5f),
-		                          new Quaternion (0, 0, 0, 0)) as GameObject;
-						chunks [x, y, z].transform.parent=gameObject.transform;
-				//blocks[x,y,z]
+						
+				chunks[x,y,z]=createChunk(x,y,z);
 							}
 	}
 	// Update is called once per frame
