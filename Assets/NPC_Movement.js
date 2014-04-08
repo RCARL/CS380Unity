3c1fb4dcc53dea5a87aea28c1440b8a9f9ecf491
RCARL <rcarl@oswego.edu>
2014-04-08 13:13:09 -0400

    Moved models into Assets directory

---
+++ Assets/NPC_Movement.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var destinations : Transform[];
+var currentNPC : CharacterController;
+var curWP : int = 0;
+function Start () {
+	currentNPC= GetComponent(CharacterController);
+}
+
+function Update () {
+	if(
+}
