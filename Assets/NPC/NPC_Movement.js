91267b08be739d5f633c9444b1e5ebb0ef2ac45b
RCARL <rcarl@oswego.edu>
2014-04-10 21:31:24 -0400

    Includes test scene with single cylinder pathing (I'm using this to add multiple objects to and debug) Includes updated patrol script with pause, turn, and variable speed included

--- Assets/NPC/NPC_Movement.js
+++ Assets/NPC/NPC_Movement.js
@@ -1,51 +1,62 @@
 ﻿#pragma strict
-
-var character : CharacterController;
-var waypoint : Transform[] = new Transform[3]; //array of locations to move through
-var movespeed : float = 10; //movement speed
-var currentWaypoint : int = 0;
-var loopthrough : boolean = true; //go through the array repeatedly
-var playerlocation : Transform;
  
-function Start ()
-{
-    character = GetComponent(CharacterController);
+var waypoint : Transform[]= new Transform[3];        // The amount of Waypoint you want
+var patrolSpeed : float = 3.0;       // The walking speed between Waypoints
+var loop : boolean = true;       // Do you want to keep repeating the Waypoints
+var dampingLook = 6.0;          // How slowly to turn
+var pauseDuration : float = 1.0;   // How long to pause at a Waypoint
+ 
+private var curTime : float;
+private var currentWaypoint : int = 0;
+private var character : CharacterController;
+ 
+function Start(){
+	character = GetComponent(CharacterController); //uses character movement methods
+    
     var start : GameObject = new GameObject();
     start.transform.position=transform.position;
-    waypoint[0]=start.transform;
+    waypoint[0]=start.transform; //the first waypoint is where the npc spawns
+    
     var first : GameObject = new GameObject();
     first.transform.position=transform.position;
-    first.transform.Translate(10,0,0,null);
+    first.transform.Translate(10,0,0,null); //the second waypoint is 10 units to the right
     waypoint[1]=first.transform;
+    
     var second : GameObject = new GameObject();
     second.transform.position=first.transform.position;
-    second.transform.Translate(0,0,10,null);
+    second.transform.Translate(0,0,10,null); //the third waypoint is 10 units down from the second wp
     waypoint[2]=second.transform;
+    //Note: those values are currently arbitrary and can be changed, this is just a sample
+    //basic movement pattern that can be repeated on a flat plane
 }
  
-function Update () 
-{
-    if(currentWaypoint < waypoint.length)
-    {
+function Update(){
+ 
+    if(currentWaypoint < waypoint.length){ //check to be within the array
+       patrol();
+       }else{    
+    if(loop){ //restarts array location for continuous patrolling
+       currentWaypoint=0;
+        } 
+    }
+}
+ 
+function patrol(){
+ 
         var target : Vector3 = waypoint[currentWaypoint].position;
-        target.y = transform.position.y; // keep waypoint at character's height
+        target.y = transform.position.y; // Keep waypoint at character's height
         var moveDirection : Vector3 = target - transform.position;
-        if(moveDirection.magnitude < 1)
-        {
-            transform.position = target; // force character to waypoint position
-            currentWaypoint++;
-        }
-        else
-        {
-            transform.LookAt(target);
-            character.Move(moveDirection.normalized * movespeed * Time.deltaTime);
-        }
-    }
-    else
-    {
-        if(loopthrough)
-        {
-            currentWaypoint=0;
-        }
-    }
+ 
+    if(moveDirection.magnitude < 0.5){
+       if (curTime == 0)
+         curTime = Time.time; // Pause over the Waypoint
+       if ((Time.time - curTime) >= pauseDuration){
+         currentWaypoint++;
+         curTime = 0;
+       }
+    }else{        
+       var rotation = Quaternion.LookRotation(target - transform.position);
+       transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * dampingLook);
+       character.Move(moveDirection.normalized * patrolSpeed * Time.deltaTime);
+    }  
 }
