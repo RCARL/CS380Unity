61dd0fd123945251b31d394a5ea2f347b786add4
Antoine Saliba <asaliba32@gmail.com>
2014-04-27 16:59:34 -0400

    test commit merge resolve

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	Vector3 movement;
+	//private float RotateSpeed = 100;
+	// Use this for initialization
+	void Start () {
+		rigidbody.useGravity = false;
+	}
+	
+	void Update () {    
+		float MoveHorizontal = Input.GetAxis("Horizontal");
+		float MoveVertical = Input.GetAxis ("Vertical");
+		float MoveUp = Input.GetAxis ("Fire2");
+		float MoveDown = Input.GetAxis ("Fire3");
+		if (Input.GetKeyDown (KeyCode.Space))
+						rigidbody.velocity = rigidbody.velocity / 1.5f;
+
+		else 
+			movement = new Vector3 (MoveHorizontal * (speed * Time.deltaTime), (MoveUp-MoveDown)*(speed * Time.deltaTime), MoveVertical * (speed * Time.deltaTime));
+			rigidbody.AddForce (movement);
+
+	}
+}
