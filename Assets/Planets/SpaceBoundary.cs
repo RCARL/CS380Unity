4b4d59a1b95748a17d8498addc3fc8f72d592ffe
Antoine Saliba <asaliba32@gmail.com>
2014-04-29 14:05:00 -0400

    None
    None

---
+++ Assets/Planets/SpaceBoundary.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceBoundary : MonoBehaviour {
+	void Update(){
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x,-9000, 7250),
+			Mathf.Clamp(transform.position.y,0,500),
+			Mathf.Clamp(transform.position.z,-2400, 1000)
+			);
+	}
+}
+
--- Assets/voxels/plopAsteroids.cs
+++ Assets/voxels/plopAsteroids.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class plopAsteroids : MonoBehaviour {
 	GameObject g;
+	public float xvalue;
+	public float yvalue;
+	public float zvalue;
 	// Use this for initialization
 	void Awake () {
 		int i = 3;
 		//for( int i = 3;i<6;i++){
 			 g = new GameObject ("asteroid"+i*0.05f);
 			g.AddComponent<Asteroids> ().thresh=i*0.05f;
-		g.transform.position= new Vector3(324.2159f,301.1146f,326.1612f);
+		g.transform.position= new Vector3(xvalue,yvalue,zvalue);
 		g.rigidbody.useGravity = true;
 		Destroy (gameObject);
 		//}
