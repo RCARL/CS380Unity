fc63498c6f6dcae2ff4e011fbc3450247ad473eb
alan12111 <Alan@dialup-129-3-132-131.oswego.edu>
2014-04-29 23:50:27 -0400

    Fixed all mining bugs
    Just attach this to a terrain for ores

--- Assets/voxels/Mine.cs
+++ Assets/voxels/Mine.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class Mine : MonoBehaviour {
-
-	//Create ores out of plain cubes
-	      
+	
+	
+	
 	public GameObject iron;
 	public GameObject copper;
 	public GameObject beryllium;
@@ -13,160 +13,261 @@ public class Mine : MonoBehaviour {
 	public GameObject platinum;
 	public GameObject uranium;
 	public GameObject plutonium;
-
-
-
+	
 	public float experience = 0.0f;
-	/*
-	 *   1. Instansiate all ores to game objects 
-	 *   2. Create a primitive cube out of all objects 
-	 *   3. Set a expiericelevel guard for each ore
-	 *   4. If user clicks copper add expierice points by 10
-	 *   5. Iron is required by at least 1000 expierince points
-	 *   6. Bery - 2000 points etc etc
-	 *   7. If ore is mined then respawn them
-     *   
-	 */  
-
-	//Just change the parameters to where ever you want to spawn the ores
-	void Start () {
-		for (int a= 0; a< 6; a++) {
-						for (int b= 0; b<6; b++) {
-
-
-								iron = GameObject.CreatePrimitive (PrimitiveType.Cube);
-								copper = GameObject.CreatePrimitive (PrimitiveType.Cube);
-								beryllium = GameObject.CreatePrimitive (PrimitiveType.Cube);
-								silver = GameObject.CreatePrimitive (PrimitiveType.Cube);
-								titanium = GameObject.CreatePrimitive (PrimitiveType.Cube);
-								platinum = GameObject.CreatePrimitive (PrimitiveType.Cube);
-								uranium = GameObject.CreatePrimitive (PrimitiveType.Cube);
-								plutonium = GameObject.CreatePrimitive (PrimitiveType.Cube);
-				 
-
-								copper.AddComponent<Rigidbody> ();
-								iron.AddComponent<Rigidbody> ();
-								beryllium.AddComponent<Rigidbody> ();
-								silver.AddComponent<Rigidbody> ();
-								titanium.AddComponent<Rigidbody> ();
-								platinum.AddComponent<Rigidbody> ();
-								uranium.AddComponent<Rigidbody> ();
-								plutonium.AddComponent<Rigidbody> ();
-
-
-
-								copper.transform.position = new Vector3 (a, b, 0);
-								iron.transform.position = new Vector3 (a, b, 0);
-								beryllium.transform.position = new Vector3 (a, b, 0);
-								silver.transform.position = new Vector3 (a, b, 0);
-								titanium.transform.position = new Vector3 (a, b, 0);
-								platinum.transform.position = new Vector3 (a, b, 0);
-								uranium.transform.position = new Vector3 (a, b, 0);
-								plutonium.transform.position = new Vector3 (a, b, 0);
-						}
-				}
-		colorOres();
-	}
-
-	void colorOres(){
-				if (GameObject.FindWithTag ("copper")) {
-						renderer.material.SetColor ("copper", Color.red);
-				} else
-		if (GameObject.FindWithTag ("iron")) {
-						renderer.material.SetColor ("iron", Color.grey);
-				} else
-		if (GameObject.FindWithTag ("beryllium")) {
-						renderer.material.SetColor ("beryllium", Color.green);
-				} else
-		if (GameObject.FindWithTag ("silver")) {
-						renderer.material.SetColor ("silver", Color.magenta);
-				} else
-		if (GameObject.FindWithTag ("titanium")) {
-						renderer.material.SetColor ("titanium", Color.white);
-				} else
-		if (GameObject.FindWithTag ("platinum")) {
-						renderer.material.SetColor ("platinum", Color.gray);
-				} else
-		if (GameObject.FindWithTag ("uranium")) {
-						renderer.material.SetColor ("uranium", Color.yellow);
-				} else
-		if (GameObject.FindWithTag ("plutonium")) {
-						renderer.material.SetColor ("plutonium", Color.cyan);
-				}
-
+	
+	void respawnCopper(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		o.AddComponent<Rigidbody> ();
+		o.tag = "copper";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.red;
+				
+			}
 			
 		}
-
+		
+	}
+	void respawnSilver(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		o.AddComponent<Rigidbody> ();
+		o.tag = "silver";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.blue;
+			}
+		}
+		
+	}
+	
+	void respawnIron(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		o.AddComponent<Rigidbody> ();
+		o.tag = "iron";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.gray;
+			}
+		}
+	}
+	void respawnBeryllium(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		o.AddComponent<Rigidbody> ();
+		o.tag = "beryllium";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.cyan;
+			}
+		}
+	}
+	void respawnTitanium(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		o.AddComponent<Rigidbody> ();
+		o.tag = "titanium";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.yellow;
+			}
+		}
+	}
+	void respawnPlatinum(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		o.AddComponent<Rigidbody> ();
+		o.tag = "platinum";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.clear;
+			}
+		}
+		o.tag = "platinum";
+	}
+	void respawnUranium(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		o.AddComponent<Rigidbody> ();
+		o.tag = "uranium";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.black;
+			}
+		}
+	}
+	void respawnPlutonium(GameObject o,int c,int d){
+		o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		//copper_clone = (GameObject)Instantiate(copper, transform.position, Quaternion.identity);
+		
+		o.AddComponent<Rigidbody> ();
+		o.tag = "plutonium";
+		
+		for (int a= 0; a< c; a++) {
+			for (int b= 0; b<d; b++) {
+				
+				o.transform.position = new Vector3 (a, b, 0);
+				o.renderer.material.color = Color.magenta;
+				
+			}
+		}
+		
+	}
+	
+	//Just change the parameters to where ever you want to spawn the ores
+	void Start () {
+		
+		respawnCopper(copper, 2, 3);
+		//		copper_clone = (GameObject)Instantiate(copper, transform.position, Quaternion.identity);
+		
+		respawnSilver (silver, 2, 5);
+		//silver_clone = (GameObject)Instantiate(silver, transform.position, Quaternion.identity);
+		
+		respawnIron (iron, 6, 4);
+		//iron_clone = (GameObject)Instantiate(silver, transform.position, Quaternion.identity);
+		
+		respawnBeryllium (beryllium, 2, 3);
+		//beryllium_clone = (GameObject)Instantiate(silver, transform.position, Quaternion.identity);
+		
+		respawnTitanium (titanium, 3, 6);
+		//titanium_clone = (GameObject)Instantiate(silver, transform.position, Quaternion.identity);
+		
+		respawnPlatinum (platinum, 2, 1);
+		//platinum_clone = (GameObject)Instantiate(silver, transform.position, Quaternion.identity);
+		
+		respawnUranium (uranium, 5, 3);
+		//uranium_clone = (GameObject)Instantiate(silver, transform.position, Quaternion.identity);
+		
+		respawnPlutonium (plutonium, 1, 4);
+		//	plutonium_clone = (GameObject)Instantiate(silver, transform.position, Quaternion.identity);
+		
+	}
+	
+	
+	
 	/*
-	 * Put this script on all ore objects, if the raycast detects we have a boolean to check
-	 * for expierience, if it is true, the object is destroyed within a certain time frame
-	 * the rarer the ore the longer it takes to mine
+	 *
 	 * 
-	 * After the ores are mined they automatically respawn
+	 * After the ores are mined just call respawn_ to bring them back
+	 * All ores are pretagged by manager in unity
 	 */
 	void Update () {
-	  
-		if (Input.GetMouseButtonDown(0)){ // if left button pressed...
-			Ray ray = new Ray();
-			ray = camera.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit = new RaycastHit();
-			if (Physics.Raycast(ray, out hit)){
-				if (GameObject.FindWithTag("copper")){
-					Destroy(copper,5);
-					experience+=5;
-					//respawn after mined
-					copper.transform.position = new Vector3(5, 0.5F, 6);
-
-				} else
-				if (GameObject.FindWithTag("iron") && experience >= 1000){
-					Destroy(iron, 8);
-					experience+=10;
-					 
-					iron.transform.position = new Vector3(5, 0.5F, 6);
-
-				} else
-				if (GameObject.FindWithTag("beryllium") && experience >= 2000){
-					Destroy(beryllium, 9);
-					experience+=30;
-
-					beryllium.transform.position = new Vector3(5, 0.5F, 6);
-				} else
-				if (GameObject.FindWithTag("silver") && experience >= 3000){
-					Destroy(beryllium, 10);
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("mouse clicked");
+			RaycastHit hitinfo = new RaycastHit();
+			bool isHit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitinfo);
+			if (isHit)
+			{
+				Debug.Log("Hit " + hitinfo.transform.gameObject.name);
+				
+				
+				// for some reason it works cube
+				if ((hitinfo.transform.gameObject.tag == "silver") && experience >= 3000)
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
 					experience+=59;
-
-					silver.transform.position = new Vector3(5, 0.5F, 6);
-				}else
-				if (GameObject.FindWithTag("titanium") && experience >= 4000){
-					Destroy(beryllium, 12);
-					experience+=90;
-
-					titanium.transform.position = new Vector3(5, 0.5F, 6);
-				}else
-				if (GameObject.FindWithTag("platinum") && experience >= 7000){
-					Destroy(beryllium, 15);
-					experience+=150;
-
-					platinum.transform.position = new Vector3(5, 0.5F, 6);
+					
+					respawnSilver(silver,2,5); 
+					
 				} else
-				if (GameObject.FindWithTag("uranium") && experience >= 9000){
-					Destroy(uranium, 21);
+					if ((hitinfo.transform.gameObject.tag == "iron") && experience >= 1000)
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
+					experience+=10;
+					
+					respawnIron(iron,2,3);
+					
+				} else
+					if (hitinfo.transform.gameObject.tag == "copper")
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
+					experience+=5;
+					
+					respawnCopper(copper,2,3);
+					
+				} else
+					
+					if ((hitinfo.transform.gameObject.tag == "beryllium") && experience >= 2000)
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
+					experience+=30;
+					
+					respawnBeryllium(beryllium,2,3);
+					
+				} 
+				else
+					if ((hitinfo.transform.gameObject.tag == "titanium") && experience >= 4000)
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
+					experience+=90;
+					
+					respawnTitanium(titanium,2,3);
+					
+				} 
+				else
+					if ((hitinfo.transform.gameObject.tag == "platinum") && experience >= 7000)
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
+					experience+=150;
+					
+					respawnPlatinum(platinum,2,3);
+					
+				} 
+				else
+					if ((hitinfo.transform.gameObject.tag == "uranium") && experience >= 9000)
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
 					experience+=200;
-
-					uranium.transform.position = new Vector3(5, 0.5F, 6);
-				}else 
-				if (GameObject.FindWithTag("plutonium") && experience >= 11000){
-					Destroy(plutonium, 33);
+					
+					respawnUranium(uranium,2,3);
+					
+				} 
+				else
+					if ((hitinfo.transform.gameObject.tag == "plutonium") && experience >= 11000)
+				{
+					Destroy(hitinfo.transform.gameObject);
+					Debug.Log("We destroyed a " + hitinfo.transform.gameObject.tag);
 					experience += 400;
-
-					plutonium.transform.position = new Vector3(5, 0.5F, 6);
-				}
-
-
-
-				Debug.Log("Insufficient experience points.");
-		    }
-	   }
-	
-   }
+					
+					respawnPlutonium(plutonium,2,3);
+					
+				} 
+				
+				
+				
+				
+				
+				
+			} else {
+				Debug.Log("Nothing hit");
+			}
+			Debug.Log("Mouse unclicked");
+		}
+		
+	}
 }
