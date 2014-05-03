27a1da9c7bbfe464a122d8a91ecb7158154c313b
alan12111 <Alan@res-dhcp-129-3-132-131.oswego.edu>
2014-05-02 21:47:39 -0400

    attach gui to ship object

--- Assets/voxels/ShipFunction.cs
+++ Assets/voxels/ShipFunction.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Threading;
 using System.Text;
@@ -6,24 +6,25 @@ using System.Globalization;
 
 public class ShipFunction : MonoBehaviour {
 	Ship user;
+	Ship target;
 	public bool hasOxygen = true;
 	public bool hasShields = true;
 	public bool clickedReformat = false;
 	static float timeLeftForOxygen = 30.0f;
 	static float timeLeftForReformat = 25.0f;
 	static float timeLeftForShields = 180.0f;
-
-
+	
+	
 	void breachSecurity(Ship target){
 		double tmp = 0.0;
-	
+		
 		int tmp2 = target.getShields ();
 		if ((target.GetSecurity() >= 50) && (hasShields)){
-		tmp = target.GetSecurity ();
-		tmp *= .85;  
-		target.newSecurity(tmp);
+			tmp = target.GetSecurity ();
+			tmp *= .85;  
+			target.newSecurity(tmp);
 			if (tmp2 > 0){
-			/*
+				/*
 			 * ******
 			Depending on the battle mechanisms, we need to see how often the other ship shoot at this ship, 
 			maybe keep it simple and let a shield break depending on how many times it got shot (100 times for one shield?)
@@ -33,8 +34,8 @@ public class ShipFunction : MonoBehaviour {
 			else{ hasShields = false; Debug.Log ("You have no shields, please create some with resources immediately (increase in health loss.");}
 		} else{  Debug.Log("Targets's security level is below 50, robot's can be controlled");       }
 	}
-
-
+	
+	
 	void hackSpeed(Ship target)
 	{
 		double tmp = 0.0;
@@ -44,9 +45,9 @@ public class ShipFunction : MonoBehaviour {
 			target.newSpeed(tmp);
 		}else { Debug.Log ("Cannot decrease below 0");} 
 	} 
-
-
-/*
+	
+	
+	/*
 Seperate timers for shields,  security, reformat, oxygen
 
 Create a check method, if no oxygen >=30 seconds, player loses
@@ -56,9 +57,9 @@ So do this in a while loop, ex: while hasoxygen = false, start a basic timer for
 Shields are the main soruce protecting their ship, so it should take longer to break a shield, maybe 3 minutes a shield because there are 10 of them.
 
 */
-
-
-
+	
+	
+	
 	void suppressOxygen(Ship target){
 		double tmp = 0.0;
 		if (target.GetOxygen() > 0) {
@@ -70,7 +71,7 @@ Shields are the main soruce protecting their ship, so it should take longer to b
 			hasOxygen = false;
 			//start timer
 		}
-
+		
 	}
 	void activateReformat(){ clickedReformat = true;}
 	void reformatSystem(Ship me){
@@ -80,7 +81,7 @@ Shields are the main soruce protecting their ship, so it should take longer to b
 		me.newOxygen(tmp2+=30.0);
 		hasOxygen = true;
 	}
-
+	
 	void OnMouseUp()
 	{
 		Debug.Log("the enemies ship will turn white once it has been clicked-released");
@@ -91,9 +92,9 @@ Shields are the main soruce protecting their ship, so it should take longer to b
 		Debug.Log("This is for targeting, the enemies ship will turn red once it has been clicked");
 		renderer.material.color = Color.red;
 	}//end method OnMouseDown
-
+	
 	void Start () {
-
+		
 	}
 	
 	void Update () {
@@ -112,8 +113,26 @@ Shields are the main soruce protecting their ship, so it should take longer to b
 			//set back to false so user can activate again
 		}
 	}
-
-
+	void OnGUI(){
+		GUI.Box (new Rect (10, 10, 150, 150), "Ship Functions");
+		if(GUI.Button(new Rect(20,40,130,20), "Hack Speed")) {
+			hackSpeed(target);
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,130,20), "Suppress Oxygen")) {
+			suppressOxygen(target);
+		}
+		if(GUI.Button(new Rect(20,100,130,20), "Reformat Ship")) {
+			activateReformat();
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(20,130,130,20), "Breach Security")) {
+			breachSecurity(target);
+		}
+	}
+	
 }
 /*
 
