a592c4e53a5d49be13797094c5445355c7c4723f
Antoine Saliba <asaliba32@gmail.com>
2014-05-03 20:34:16 -0400

    None
    None

--- Assets/Planets/SpaceBoundary.cs
+++ Assets/Planets/SpaceBoundary.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpaceBoundaryg : MonoBehaviour {
+public class SpaceBoundary : MonoBehaviour {
+	public float xmin;
+	public float xmax;
+	public float ymin;
+	public float ymax;
+	public float zmin;
+	public float zmax;
+
 	void Update(){
 		transform.position = new Vector3(
-			Mathf.Clamp(transform.position.x,-9000, 7250),
-			Mathf.Clamp(transform.position.y,0,500),
-			Mathf.Clamp(transform.position.z,-2400, 1000)
+			Mathf.Clamp(transform.position.x,xmin, xmax),
+			Mathf.Clamp(transform.position.y,ymin, ymax),
+			Mathf.Clamp(transform.position.z,zmin, zmax)
 			);
 	}
 }
