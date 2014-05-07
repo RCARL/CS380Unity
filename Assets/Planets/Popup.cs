58998c4f95a24cc1bb82473603ddf00c36e9b98e
Antoine Saliba <asaliba32@gmail.com>
2014-05-06 22:26:51 -0400

    None
    None

---
+++ Assets/Planets/Popup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Popup : MonoBehaviour {
+	public bool poppedUp;
+	public static string map = "AsteroidsTestScene";
+	public static Vector3 defaultRespawn = new Vector3(-1413.0f,229.0f,-1000.0f);
+	// Use this for initialization
+	void Start () {
+		poppedUp = false;
+	}
+
+	void OnGUI () {
+		if (Input.GetKeyDown ("h"))
+						poppedUp = true;
+		if (poppedUp) {
+			GUI.Box(new Rect((Screen.width / 2) - Screen.width / 8, (Screen.height / 2) - Screen.height / 8, Screen.width / 4, Screen.height / 6), "Leave Planet?");
+			if (GUI.Button (new Rect ((Screen.width / 2) - Screen.width / 10, (Screen.height / 2) - Screen.height / 18, Screen.width / 12, Screen.height / 18), "Yes")) {
+				poppedUp = false;
+				map = Application.loadedLevelName;
+				Debug.Log(map);
+				Application.LoadLevel ("AsteroidsTestScene");
+			}
+			if (GUI.Button (new Rect ((Screen.width / 2) + Screen.width / 50, (Screen.height / 2) - Screen.height / 18, Screen.width / 12, Screen.height / 18), "No")) {
+				poppedUp = false;
+				//Code for when no is hit goes here
+			}
+		}
+	}
+}
--- Assets/Player/ActiveSpawnPoint.cs
+++ Assets/Player/ActiveSpawnPoint.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class ActiveSpawnPoint : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
-		if (ChangeS.map == "Forest") {
+		if (Popup.map == "Forest") {
 						transform.position = new Vector3 (327.0f, 229.0f, 3091.0f);
-				} else if (ChangeS.map == "Island") {
+				} else if (Popup.map == "Island") {
 						transform.position = new Vector3 (-7088.0f, 229.0f, 3091.0f);
-				} else if (ChangeS.map == "Volcano") {
+				} else if (Popup.map == "volcano") {
 						transform.position = new Vector3 (6704.0f, 229.0f, 1875.0f);
 				}else
-						transform.position = ChangeS.defaultRespawn;
+						transform.position = Popup.defaultRespawn;
 
 	}
 	
